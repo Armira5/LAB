0362cf22df6ad4cc705d5927f5509bf50c208583
Armira5 <93438741+Armira5@users.noreply.github.com>
2023-04-26 23:53:27 +0200

    Add project files.

---
+++ BookCommerceCustom1/Models/Kompania.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BookCommerceCustom1.Models
+{
+  
+    public class Kompania
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Emri { get; set; }
+        public string? Rruga { get; set; }
+        public string? Qyteti { get; set; }
+        public string? Shteti { get; set; }
+        public string? KodiPostal { get; set; }
+        public string? NumriTelefonit { get; set; }
+    }
+}
