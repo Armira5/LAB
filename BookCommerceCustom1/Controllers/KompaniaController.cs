cbdef01a673a8b84fa55625a18ac73313ee5af2d
Armira5 <93438741+Armira5@users.noreply.github.com>
2023-05-05 13:08:00 +0200

    Controllers

---
+++ BookCommerceCustom1/Controllers/KompaniaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCommerceCustom1.Controllers
+{
+	public class KompaniaController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ BookCommerceCustom1/Controllers/MbulesaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookCommerceCustom1.Controllers
+{
+	public class MbulesaController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
